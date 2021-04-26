462f37c2cab81976a1d85947ee04c463df90d5ec
Brenna Moore <brenna@brenna.dev>
2021-04-25 17:32:57 -0700

    add host args

--- SkyWeather2Slim.py
+++ SkyWeather2Slim.py
@@ -1,13 +1,14 @@
 ﻿import paho.mqtt.client as mqtt
 import time
 import sys
+import getopt
 import json
 import SkyWeather2Parser
 
 
-def main():
+def main(host):
     client = mqtt.Client()
-    client.connect("192.168.50.147", 1883 , 60)
+    client.connect(host, 1883 , 60)
     client.loop_start()
 
     for line in sys.stdin:
@@ -18,4 +19,17 @@ def main():
     client.loop_stop()
 
 if __name__ == "__main__":
-    main()
+    short_options = "h:"
+    long_options = ["host="]
+    host = "localhost"
+    try:
+        full_cmd_arguments = sys.argv
+        argument_list = full_cmd_arguments[1:]
+        arguments, values = getopt.getopt(argument_list, short_options, long_options)
+        for argument, value in arguments:
+            if (argument in ("-h", "--host")):
+                host = value
+    except getopt.error as err:
+        print(str(err))
+        sys.exit(2)
+    main(host)
