c45540c7e1799842b6e837378f4f2677c130c37f
brenna <brenna+git@brenna.dev>
2021-04-25 17:10:22 -0700

    wip

---
+++ SkyWeather2Parser.py
@@ -0,0 +1,39 @@
+﻿#
+# Parse the JSON blob from rtl_433 into usable weather data
+#
+
+def parseFromRtl433(rtlJson):
+    
+    parsedTemperature = round(((((rtlJson["temperature"] - 400)/10.0) - 32.0)/(9.0/5.0)),2)
+    parsedLight = rtlJson["light"]
+    if (parsedLight >= 0x1fffa):
+        parsedLight = parsedLight | 0x7fff0000
+
+    parsedUvIndex =rtlJson["uv"]
+    if (parsedUvIndex >= 0xfa):
+        parsedUvIndex = parsedUvIndex | 0x7f00
+    
+    return {
+        "timestamp": rtlJson["time"],
+        "temperature": parsedTemperature,
+        "humidity": rtlJson["humidity"],
+        "sunlight": {
+            "visible": parsedLight,
+            "uvIndex": round(parsedUvIndex/10.0, 1 )
+        },
+        "wind": {
+            "speed": round(rtlJson["avewindspeed"]/10.0, 1),
+            "gust": round(rtlJson["gustwindspeed"]/10.0, 1),
+            "direction": rtlJson["winddirection"]
+        },
+        "rain": {
+          "total": round(rtlJson["cumulativerain"]/10.0,1)  
+        },
+        "barometric": {
+            "temperature": 0,
+            "pressure": 0,
+            "trend": 0
+        },
+        "aqi": 0,
+        "battery": "OK" if rtlJson['batterylow'] == 0 else "LOW"
+    }
---
+++ SkyWeather2Slim.py
@@ -0,0 +1,21 @@
+﻿import paho.mqtt.client as mqtt
+import time
+import sys
+import json
+import SkyWeather2Parser
+
+
+def main():
+    client = mqtt.Client()
+    client.connect("192.168.50.147", 1883 , 60)
+    client.loop_start()
+
+    for line in sys.stdin:
+        if ("FT020T" in line):
+            client.publish("sw2/data", SkyWeather2Parser.parseFromRtl433(json.loads(line)))
+
+    time.sleep(2)
+    client.loop_stop()
+
+if __name__ == "__main__":
+    main()
